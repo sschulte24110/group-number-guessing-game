85bfe7086abdf3c7f2cde17b8286350954652303
Abdi <abdullahia0215@gmail.com>
2024-05-09 15:47:38 -0500

    added renderGuessHistory function, added sendGuesses function, sends correct table to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,54 @@
 function onReady() {
-  console.log("JavaScript is loaded!")
+  console.log("JavaScript is loaded!");
+  renderGuessHistory();
+}
+const historyDiv = document.getElementById("tbody");
+
+function sendGuesses(event) {
+  event.preventDefault();
+
+  const playerOneGuess = document.getElementById("player-one-guess").value;
+  const playerTwoGuess = document.getElementById("player-two-guess").value;
+  const playerThreeGuess = document.getElementById("player-three-guess").value;
+
+  axios({
+    method: "post",
+    url: `/guesses`,
+    data: {
+      playerOneGuess: Number(playerOneGuess),
+      playerTwoGuess: Number(playerTwoGuess),
+      playerThreeGuess: Number(playerThreeGuess),
+    },
+  })
+    .then(function (response) {
+      console.log("SUCCESS");
+      renderGuessHistory();
+
+      document.getElementById("player-one-guess").value = "";
+      document.getElementById("player-two-guess").value = "";
+      document.getElementById("player-three-guess").value = "";
+    })
+    .catch(function (response) {
+      alert("request failed");
+      console.log(error);
+    });
 }
 
-onReady()
+function renderGuessHistory() {
+  axios({
+    method: "GET",
+    url: "/guesses",
+  }).then(function (response) {
+    historyDiv.innerHTML = "";
+    let guesses = response.data;
+    for (let players of guesses) {
+      historyDiv.innerHTML += `<tr>
+      <td>${players.playerOneGuess}</td>
+      <td>${players.playerTwoGuess}</td>
+      <td>${players.playerThreeGuess}</td>
+      </tr>`;
+    }
+  });
+}
+
+onReady();
